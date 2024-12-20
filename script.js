bdbe33e369add5bf96346236beb2ef13295a38dc
OsamuChuuya <osamuchuuya26@gmail.com>
2024-12-20 17:48:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let name1 = prompt(`ismingizni kiriting`)
+let harf = prompt(`harf kiriting`)
+if(name1.includes (harf)){
+    alert(`${name1} ismning ichida ${harf} harifi bor`)
+
+}
+else{
+    alert(`${name1} ismning ichida ${harf} harifi yoq`)
+}
